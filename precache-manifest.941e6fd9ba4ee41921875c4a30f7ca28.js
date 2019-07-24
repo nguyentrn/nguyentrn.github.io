168788ddeaeae37d97f7b5f927c8a405dbac8674
nguyentrn <nguyentran0113@gmail.com>
2019-07-25 01:23:51 +0700

    addd

---
+++ precache-manifest.941e6fd9ba4ee41921875c4a30f7ca28.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "66ea5bd9fc22dc465921d0e38d5215b3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "db11c2285990616c3bca",
+    "url": "/static/css/main.57dfdda0.chunk.css"
+  },
+  {
+    "revision": "b967683a96834dc12d1d",
+    "url": "/static/js/2.f24d5b82.chunk.js"
+  },
+  {
+    "revision": "db11c2285990616c3bca",
+    "url": "/static/js/main.6a673803.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
